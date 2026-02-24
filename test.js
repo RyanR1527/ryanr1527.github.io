9523f0096ac7ad324850acd118fb12dc06b18a0d
Ryan Razzano <ryanrazz1@icloud.com>
2026-02-24 14:22:16 +0000

    fixed script prompt

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
 let a ="hello";
     console.log("Hello world");
     alert("Hello world");
+
+const buttonA=document.querySelector("#button_A");
+
+const headingA=document.getElementById("heading_A")
+
+buttonA.addEventListener('click', function() {
+
+const name=prompt("What is your name?");
+
+alert(`Hello ${name}`);
+headingA.textContent=`Welcome ${name}`;
+});
+
